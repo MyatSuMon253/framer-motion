de8228d964ef27304aee4d68a0477b66beefef7d
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-02-24 00:46:07 +0630

    interaction

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <main className="h-screen flex items-center justify-center">
-      <motion.div
-        className="bg-black w-40 h-40"
-        animate={{
-          scale: [1, 2, 2.5, 2, 1],
-          rotate: [0, 0, 100, 0, 0],
-          borderRadius: ["0%", "50%", "0%"],
-        }}
-        transition={{
-          delay: 2,
-          repeat: Infinity,
-          repeatDelay: 1,
-          duration: 2,
-          times: [0, 0.1, 0.2, 0.5],
-        }}
-      ></motion.div>
+      <motion.button
+        className="bg-black text-white p-4 rounded-md font-mono"
+        whileHover={{ scale: 2 }}
+        whileTap={{ scale: 2.5 }}
+      >
+        Click Me!
+      </motion.button>
     </main>
   );
 }
