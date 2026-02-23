30f05a08840dd87eeed961292991ee552fa0d7ec
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-02-24 00:40:31 +0630

    transition prop

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import { motion } from "framer-motion";
 
 function App() {
   return (
-    <main className="m-10">
+    <main className="h-screen flex items-center justify-center">
       <motion.div
-        className="bg-black px-3 py-2 rounded-md text-red-500 text-center w-40 h-20"
-        animate={{ x: "500px", y: "200px" }}
-      >
-        Hello World
-      </motion.div>
+        className="bg-black w-40 h-40"
+        animate={{
+          scale: [1, 2, 2.5, 2, 1],
+          rotate: [0, 0, 100, 0, 0],
+          borderRadius: ["0%", "50%", "0%"],
+        }}
+        transition={{
+          delay: 2,
+          repeat: Infinity,
+          repeatDelay: 1,
+          duration: 2,
+          times: [0, 0.1, 0.2, 0.5],
+        }}
+      ></motion.div>
     </main>
   );
 }
