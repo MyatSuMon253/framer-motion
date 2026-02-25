8c83a8a064bef2211b5fa5e9f5850fbb82872bf2
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-02-25 10:20:30 +0630

    drag and change color

--- src/components/draggable-box.tsx
+++ src/components/draggable-box.tsx
@@ -4,13 +4,17 @@ const DraggableBox = () => {
   const y = useMotionValue(0);
   const scale = useTransform(y, [-100, 0, 100], [1.6, 1, 1.6]);
   const borderRadius = useTransform(y, [-100, 0, 100], ["50%", "0%", "50%"]);
-
+  const backgroundColor = useTransform(
+    y,
+    [-100, 0, 100],
+    ["#219FFC", "#99FFCE", "#219FFC"],
+  );
   return (
     <motion.div
       className="bg-black h-20 w-20 rounded-md"
       drag="y"
       dragConstraints={{ top: 0, bottom: 0 }}
-      style={{ y, scale, borderRadius }}
+      style={{ y, scale, borderRadius, backgroundColor }}
     />
   );
 };
